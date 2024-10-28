d99027cdab5866365aae38c0e7f6b7abaf7cb7a4
Muhaimin Shihab <talk.shihab@gmail.com>
2024-10-28 17:01:11 +0600

    add: factory and seeder for teacher

---
+++ database/factories/TeacherFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Teacher>
+ */
+class TeacherFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'teacher_id' => fake()->numberBetween(100000, 999999),
+            'school_name' => fake()->company(),
+            'nid_number' => fake()->numberBetween(10000000, 99999999),
+            'address' => fake()->address(),
+            'emergency_contact' => json_encode(
+                [
+                    'name' => fake()->name(),
+                    'phone' => fake()->phoneNumber()
+                ]
+            )
+        ];
+    }
+}
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Student;
+use App\Models\Teacher;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +15,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create an admin user
         User::create([
             'name' => 'Admin',
             'phone' => '1234567890',
@@ -22,12 +24,24 @@ class UserSeeder extends Seeder
             'user_type' => 'admin'
         ]);
 
-        // Create fake users
-        $users = User::factory(50)->create();
+        // Create fake students
+        $students = User::factory(50)->create([
+            'user_type' => 'student'
+        ]);
 
-        // Create a student for each user
-        $users->each(function ($user) {
-            Student::factory()->create(['user_id' => $user->id]);
+        // Create a student record for each student user
+        $students->each(function ($studentUser) {
+            Student::factory()->create(['user_id' => $studentUser->id]);
+        });
+
+        // Create fake teachers
+        $teachers = User::factory(50)->create([
+            'user_type' => 'teacher'
+        ]);
+
+        // Create a teacher record for each teacher user
+        $teachers->each(function ($teacherUser) {
+            Teacher::factory()->create(['user_id' => $teacherUser->id]);
         });
     }
 }
