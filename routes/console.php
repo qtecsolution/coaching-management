e907270670c3e195340c2edd797bb1279cef1fb2
Azizul Hoque <azizulh8774@gmail.com>
2024-12-02 15:07:04 +0600

    updated schedular

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(GenerateMonthlyPayments::class)->everySecond();
+Schedule::command(GenerateMonthlyPayments::class)->monthlyOn(1, '00:00');
