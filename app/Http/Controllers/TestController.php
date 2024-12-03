2145afcf7466484802ab1021d77e53c5bcfc0027
Muhaimin Shihab <talk.shihab@gmail.com>
2024-12-03 19:00:12 +0600

    Remove unusual code from the test controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -20,30 +20,6 @@ class TestController extends Controller
         //     return $th->getMessage();
         // }
 
-        // $sender = Sender::getInstance();
-        // $sender->setProvider(Mobireach::class);
-        // $sender->setMobile(['01829004863']);
-        // $sender->setMessage('Hello World!');
-        // $sender->setConfig(
-        //     [
-        //         'username' => env('SMS_MOBIREACH_USERNAME'),
-        //         'password' => env('SMS_MOBIREACH_PASSWORD'),
-        //         'sender' => env('SMS_MOBIREACH_SENDER'),
-        //         'contentType' => env('SMS_MOBIREACH_CONTENT_TYPE'),
-        //         'msgType' => env('SMS_MOBIREACH_MSG_TYPE'),
-        //         'requestType' => env('SMS_MOBIREACH_REQUEST_TYPE'),
-        //     ],
-        // );
-        // $status = $sender->send();
-        // return $status;
-
-        // try {
-        //     $smsService = new AdaReachService();
-        //     return $smsService->sendSMS('01829004863', 'Hello World!');
-        // } catch (\Throwable $th) {
-        //     return $th->getMessage();
-        // }
-
         return response()->json([
             'message' => 'Hello World!',
             'start_url' => route('auth.login.show'),
