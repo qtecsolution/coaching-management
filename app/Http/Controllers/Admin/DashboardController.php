7b7c47f8b0cba545cc1f7dd37948093731ce1de6
Md Muhaiminul Islam Shihab <talk.shihab@gmail.com>
2025-02-16 16:51:11 +0600

    Show statistics on the admin dashboard.

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Batch;
 use App\Models\BatchDayDate;
+use App\Models\Lead;
+use App\Models\Payment;
+use App\Models\Student;
 use App\Models\Teacher;
 
 class DashboardController extends Controller
@@ -24,6 +28,11 @@ class DashboardController extends Controller
             return view('teacher.dashboard', compact('user', 'classSchedules'));
         }
 
-        return view('admin.dashboard');
+        $totalBatch = Batch::active()->count();
+        $totalStudent = Student::active()->count();
+        $totalPayment = Payment::sum('amount');
+        $totalLead = Lead::count();
+
+        return view('admin.dashboard', compact('totalBatch', 'totalStudent', 'totalPayment', 'totalLead'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,18 +15,17 @@
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-4 d-flex justify-content-start ">
-                                        <div class="stats-icon red mb-2">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                width="24" height="24" color="#FFFFFF" stroke-width="1.5"
-                                                stroke="currentColor" class="size-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+                                        <div class="stats-icon purple mb-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                color="#FFFFFF" fill="currentColor" class="bi bi-easel" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M8 0a.5.5 0 0 1 .473.337L9.046 2H14a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1.85l1.323 3.837a.5.5 0 1 1-.946.326L11.092 11H8.5v3a.5.5 0 0 1-1 0v-3H4.908l-1.435 4.163a.5.5 0 1 1-.946-.326L3.85 11H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h4.954L7.527.337A.5.5 0 0 1 8 0M2 3v7h12V3z" />
                                             </svg>
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-8">
-                                        <h6 class="text-muted font-semibold">Total Students</h6>
-                                        <h6 class="font-extrabold mb-0">{{ @$data['total_student'] }}</h6>
+                                        <h6 class="text-muted font-semibold">Running Batch</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $totalBatch }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -37,18 +36,18 @@
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-4 d-flex justify-content-start ">
-                                        <div class="stats-icon purple mb-2">
+                                        <div class="stats-icon red mb-2">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 width="24" height="24" color="#FFFFFF" stroke-width="1.5"
                                                 stroke="currentColor" class="size-6">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+                                                    d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
                                             </svg>
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-8">
-                                        <h6 class="text-muted font-semibold">Total Collectable</h6>
-                                        <h6 class="font-extrabold mb-0">{{ @$data['total_estimated_collection'] }}</h6>
+                                        <h6 class="text-muted font-semibold">Active Student</h6>
+                                        <h6 class="font-extrabold mb-0">{{ number_format($totalStudent) }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -60,17 +59,21 @@
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-4 d-flex justify-content-start ">
                                         <div class="stats-icon blue mb-2">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                width="24" height="24" color="#FFFFFF" stroke-width="1.5"
-                                                stroke="currentColor" class="size-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                color="#FFFFFF" fill="currentColor" class="bi bi-clipboard2-data"
+                                                viewBox="0 0 16 16">
+                                                <path
+                                                    d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5z" />
+                                                <path
+                                                    d="M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5z" />
+                                                <path
+                                                    d="M10 7a1 1 0 1 1 2 0v5a1 1 0 1 1-2 0zm-6 4a1 1 0 1 1 2 0v1a1 1 0 1 1-2 0zm4-3a1 1 0 0 0-1 1v3a1 1 0 1 0 2 0V9a1 1 0 0 0-1-1" />
                                             </svg>
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-8">
-                                        <h6 class="text-muted font-semibold">Total Collected</h6>
-                                        <h6 class="font-extrabold mb-0">{{ @$data['total_collection'] }}</h6>
+                                        <h6 class="text-muted font-semibold">Total Lead</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $totalLead }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -91,8 +94,8 @@
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-8">
-                                        <h6 class="text-muted font-semibold">Total Due</h6>
-                                        <h6 class="font-extrabold mb-0">{{ @$data['total_due'] }}</h6>
+                                        <h6 class="text-muted font-semibold">Total Payment</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $totalPayment }}</h6>
                                     </div>
                                 </div>
                             </div>
